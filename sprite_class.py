33aaa1b62c5d4ae829df498da699fc6876c10447
SinD:/Users/Administrator/AppData/Local/Programs/Python/Python38/python.exe <veronikavoss.none@gmail.comD:/Users/Administrator/AppData/Local/Programs/Python/Python38/python.exe>
2022-02-01 12:54:11 +0900

    collision

--- sprite_class.py
+++ sprite_class.py
@@ -1,11 +1,11 @@
 from setting import *
 
 class SurfaceSprite(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self,topleft):
         super().__init__()
         self.image=pygame.Surface((30,40))
         self.image.fill('blue')
-        self.rect=self.image.get_rect()
+        self.rect=self.image.get_rect(topleft=topleft)
     
     def move(self):
         pass
